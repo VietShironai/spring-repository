4fb7d4075aed47466fa154d5e7a6cf26a0bb953a
shironai <huybohoho37@gmail.com>
2019-07-17 09:46:02 +0700

    add github

---
+++ src/main/java/com/codegym/cms/service/impl/CustomerServiecImpl.java
@@ -0,0 +1,29 @@
+package com.codegym.cms.service.impl;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiecImpl implements CustomerService {
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Customer customer) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+}
